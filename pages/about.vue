902fc8493df8ef4b37928682494ffd5699e7f334
wenduidui <7827453+wenduidui@user.noreply.gitee.com>
2020-11-25 16:31:01 +0800

    FIRST DEMO

---
+++ pages/about.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <h1>About Page</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'aboutPage',
+  data () {
+    return {
+    }
+  },
+  methods: {}
+}
+</script>
+
+<style lang='less'>
+
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <h1>Hello NuxtJS</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HomePage',
+  data () {
+    return {
+    }
+  },
+  methods: {}
+}
+</script>
+
+<style lang='less'>
+
+</style>
