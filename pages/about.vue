fa5aaea2ff4d6da7be58a70c2500805f3767c4d5
wenduidui <7827453+wenduidui@user.noreply.gitee.com>
2020-11-25 17:01:33 +0800

    路由导航

--- pages/about.vue
+++ pages/about.vue
@@ -1,6 +1,17 @@
 <template>
   <div>
     <h1>About Page</h1>
+    <!-- a 链接,刷新导航,走服务端渲染-->
+    <h2>a 链接</h2>
+    <a href="/">首页</a>
+
+    <!-- router-link 导航链接组件 -->
+    <h2>router-link</h2>
+    <router-link to="/">首页</router-link>
+
+    <!-- 编程式导航 -->
+    <h2>编程式导航</h2>
+    <button @click="onClick">首页</button>
   </div>
 </template>
 
@@ -11,7 +22,12 @@ export default {
     return {
     }
   },
-  methods: {}
+  methods: {
+    onClick () {
+      console.log('hello')
+      this.$router.push('/')
+    }
+  }
 }
 </script>
 
