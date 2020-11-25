57f1252a7e6c388f94401ce08644690edf345561
wenduidui <7827453+wenduidui@user.noreply.gitee.com>
2020-11-25 16:45:22 +0800

    branch

---
+++ pages/user/abc.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <h1>User-ABC page</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'UserAbc',
+  data () {
+    return {
+    }
+  },
+  methods: {}
+}
+</script>
+
+<style lang='less'>
+
+</style>
---
+++ pages/user/index.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <h1>User Page</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'UserPage',
+  data () {
+    return {
+    }
+  },
+  methods: {}
+}
+</script>
+
+<style lang='less'>
+
+</style>
